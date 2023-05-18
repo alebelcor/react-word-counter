bc905fda78e6bfed5b2b8901847b5b5ca81d7ce9
alebelcor <856838+alebelcor@users.noreply.github.com>
2023-05-18 13:39:20 -0700

    initial commit

---
+++ readme.md
@@ -0,0 +1,21 @@
+# React Word Counter
+
+>Word counter. An application to count words. Count how many words there are in a text
+
+## Requirements
+
+* [Node.js](https://nodejs.org/) 16+
+
+## Usage
+
+### Install dependencies
+
+```bash
+npm install
+```
+
+### Start a development server
+
+```bash
+npm run dev
+```
---
+++ src/app/page.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { useState } from "react"
+
+export default function Home() {
+  const [content, setContent] = useState('')
+  const [wordCount, setWordCount] = useState(0)
+
+  return (
+    <main className="py-4 px-8 flex flex-col items-center gap-8">
+      <h1 className="text-2xl font-semibold text-center">Word Counter</h1>
+
+      <textarea
+        className="p-4 w-full max-w-4xl resize-y border border-black text-black "
+        name="text"
+        value={content}
+        onChange={(event) => {
+          const content = event.target.value ?? '';
+
+          setContent(content);
+          setWordCount(content.replace(/[.,;:\-–—\/]/g, ' ').split(' ').filter(Boolean).length)
+        }}
+      />
+
+      {wordCount > 0 ? wordCount : undefined}
+    </main>
+  )
+}
